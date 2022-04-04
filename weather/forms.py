8344972164e4268593deef0a733baa759cd95cd7
irbaiev <a.irbaeiv75@gmail.com>
2022-04-04 17:33:14 +0300

    create project

---
+++ weather/forms.py
@@ -0,0 +1,11 @@
+from .models import City
+from django.forms import ModelForm, TextInput
+
+
+class CityForm(ModelForm):
+    class Meta():
+        model = City
+        fields = ['name']
+        vidgets = {'name': TextInput(attrs={
+            'class':'form-control','name':'city','aria-label':'Sizing example input',
+            ' aria-describedby':'inputGr','id':'city','placeholder':'Укажите город'})}
